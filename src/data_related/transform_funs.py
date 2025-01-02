152c19ec795a24cc85eda415c6f86c7fdfd3f384
zfq <zhangfengqianbupt@bupt.edu.cn>
2025-01-02 21:15:30 +0800

    添加对于深度图致密化的代码逻辑

--- src/data_related/transform_funs.py
+++ src/data_related/transform_funs.py
@@ -6,6 +6,75 @@ from torchvision import transforms
 
 from data_related.converter import Converter
 
+# Full kernels
+FULL_KERNEL_3 = np.ones((3, 3), np.uint8)
+FULL_KERNEL_5 = np.ones((5, 5), np.uint8)
+FULL_KERNEL_7 = np.ones((7, 7), np.uint8)
+FULL_KERNEL_9 = np.ones((9, 9), np.uint8)
+FULL_KERNEL_31 = np.ones((31, 31), np.uint8)
+
+# 3x3 cross kernel
+CROSS_KERNEL_3 = np.asarray(
+    [
+        [0, 1, 0],
+        [1, 1, 1],
+        [0, 1, 0],
+    ],
+    dtype=np.uint8,
+)
+
+# 5x5 cross kernel
+CROSS_KERNEL_5 = np.asarray(
+    [
+        [0, 0, 1, 0, 0],
+        [0, 0, 1, 0, 0],
+        [1, 1, 1, 1, 1],
+        [0, 0, 1, 0, 0],
+        [0, 0, 1, 0, 0],
+    ],
+    dtype=np.uint8,
+)
+
+# 5x5 diamond kernel
+DIAMOND_KERNEL_5 = np.array(
+    [
+        [0, 0, 1, 0, 0],
+        [0, 1, 1, 1, 0],
+        [1, 1, 1, 1, 1],
+        [0, 1, 1, 1, 0],
+        [0, 0, 1, 0, 0],
+    ],
+    dtype=np.uint8,
+)
+
+# 7x7 cross kernel
+CROSS_KERNEL_7 = np.asarray(
+    [
+        [0, 0, 0, 1, 0, 0, 0],
+        [0, 0, 0, 1, 0, 0, 0],
+        [0, 0, 0, 1, 0, 0, 0],
+        [1, 1, 1, 1, 1, 1, 1],
+        [0, 0, 0, 1, 0, 0, 0],
+        [0, 0, 0, 1, 0, 0, 0],
+        [0, 0, 0, 1, 0, 0, 0],
+    ],
+    dtype=np.uint8,
+)
+
+# 7x7 diamond kernel
+DIAMOND_KERNEL_7 = np.asarray(
+    [
+        [0, 0, 0, 1, 0, 0, 0],
+        [0, 0, 1, 1, 1, 0, 0],
+        [0, 1, 1, 1, 1, 1, 0],
+        [1, 1, 1, 1, 1, 1, 1],
+        [0, 1, 1, 1, 1, 1, 0],
+        [0, 0, 1, 1, 1, 0, 0],
+        [0, 0, 0, 1, 0, 0, 0],
+    ],
+    dtype=np.uint8,
+)
+
 
 def img_transform(resolution):
     augmentations = transforms.Compose([
@@ -70,10 +139,77 @@ def _resize_img(img, resolution):
     return img
 
 
+def fill_in_fast(depth_map, max_depth=15.0, custom_kernel=DIAMOND_KERNEL_7, extrapolate=False, blur_type="bilateral"):
+    """
+    Fast, in-place depth completion.
+
+    Args:
+    depth_map: projected depths
+    max_depth: max depth value for inversion
+    custom_kernel: kernel to apply initial dilation
+    extrapolate: whether to extrapolate by extending depths to top of
+    the frame, and applying a 31x31 full kernel dilation
+    blur_type:
+    'bilateral' - preserves local structure (recommended)
+    'gaussian' - provides lower RMSE
+
+    Returns:
+    depth_map: dense depth map
+    """
+
+    # Invert
+    valid_pixels = depth_map > 0.1
+    depth_map[valid_pixels] = max_depth - depth_map[valid_pixels]
+
+    # Dilate
+    depth_map = cv2.dilate(depth_map, custom_kernel)
+
+    # Hole closing
+    depth_map = cv2.morphologyEx(depth_map, cv2.MORPH_CLOSE, FULL_KERNEL_5)
+
+    # Fill empty spaces with dilated values
+    empty_pixels = depth_map < 0.1
+    dilated = cv2.dilate(depth_map, FULL_KERNEL_7)
+    depth_map[empty_pixels] = dilated[empty_pixels]
+
+    # Extend highest pixel to top of image
+    if extrapolate:
+        top_row_pixels = np.argmax(depth_map > 0.1, axis=0)
+        top_pixel_values = depth_map[top_row_pixels, range(depth_map.shape[1])]
+
+        for pixel_col_idx in range(depth_map.shape[1]):
+            depth_map[0 : top_row_pixels[pixel_col_idx], pixel_col_idx] = top_pixel_values[pixel_col_idx]
+
+        # Large Fill
+        empty_pixels = depth_map < 0.1
+        dilated = cv2.dilate(depth_map, FULL_KERNEL_31)
+        depth_map[empty_pixels] = dilated[empty_pixels]
+
+    # Median blur
+    depth_map = cv2.medianBlur(depth_map, 5)
+
+    # Bilateral or Gaussian blur
+    if blur_type == "bilateral":
+        # Bilateral blur
+        depth_map = cv2.bilateralFilter(depth_map, 5, 1.5, 2.0)
+    elif blur_type == "gaussian":
+        # Gaussian blur
+        valid_pixels = depth_map > 0.1
+        blurred = cv2.GaussianBlur(depth_map, (5, 5), 0)
+        depth_map[valid_pixels] = blurred[valid_pixels]
+
+    # Invert
+    valid_pixels = depth_map > 0.1
+    depth_map[valid_pixels] = max_depth - depth_map[valid_pixels]
+
+    return depth_map
+
+
 def dpt_transform(resolution):
     def transform(pc):
         # 投影成深度图
         dpt = Converter.proj_pc2dpt(pc, extrinsic=np.eye(4), intrinsic=np.eye(3), h=resolution, w=resolution)
+        dpt = fill_in_fast(dpt.astype(np.float32)) # 对深度图进行致密化, 之前写代码的时候忘记把这一段逻辑抄上了
         # 对深度图的大小进行调整
         dpt = np.array(_resize_img(_HWC3(_depth_normalize(dpt)), resolution))
         # 转换成 Tensor 并进行维度调整
